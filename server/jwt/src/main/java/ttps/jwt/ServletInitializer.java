10ab8a1c1951a467c9821e205eadec254070eac6
Manuel Ortiz <manuel.ortiz@fluxit.com.ar>
2018-12-11 17:51:55 -0300

    primer commit

---
+++ server/jwt/src/main/java/ttps/jwt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ttps.jwt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JwtApplication.class);
+	}
+
+}
